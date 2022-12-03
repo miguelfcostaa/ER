a297331a4d9f134881f0be5d0fee90a3506fcbed
costaaaj <miguelcosta220@hotmail.com>
2022-12-03 11:33:50 +0000

    update

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+server.listen(port, () => {
+    console.log(`Application is running at: http://localhost:${port}`);
+    console.log('Connected!');
+})
+
+
+app.get('/', (res,req)=> {
+    res.sendFile(__dirname + "/html/login.html");
+});
+
+
 app.use((req,res) => {
     res.status(404).render('404');
 });
