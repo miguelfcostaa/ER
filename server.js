7b1d44733d29be8686a129f091c72d597f9c5f68
costaaaj <miguelcosta220@hotmail.com>
2022-12-03 12:04:11 +0000

    update

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-const mongoose = require('mongoose');
+
 const http = require('http');
 const express = require('express');
 const session = require("express-session");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+
+
 
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
-
-const sessionMiddleware = session({ secret: "changeit", resave: false, saveUninitialized: false });
-
-app.use(sessionMiddleware);
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json())
-app.use(passport.initialize());
-app.use(passport.session());
 
 
 server.listen(port, () => {
@@ -31,7 +22,13 @@ app.get('/', (req,res)=> {
     res.sendFile(__dirname + "/html/main.html");
 });
 
+app.get('/ecraPrincipal', (req,res)=> {
+    res.sendFile(__dirname + "/html/ecraPrincipal.html");
+});
 
+app.get('/ecraVolante', (req,res)=> {
+    res.sendFile(__dirname + "/html/ecraVolante.html");
+});
 
 
 app.use((req,res) => {
